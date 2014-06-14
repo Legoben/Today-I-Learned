3c4c2528febe67536f0575d0b461ab0f05583328
MaxWofford <max.speed.wofford@gmail.com>
2014-06-13 22:45:44 -0700

    Git Real

--- www/js/dashboard.js
+++ www/js/dashboard.js
@@ -2,8 +2,9 @@
 var populate = function() {
 
     for (var i = 1; i < 50 ; i++) {
-        var listing = "<li id=\"" + i + "\">Today I learned <span id=\"til" + i + "\"><a href=\"http://thecatapi.com\"><img src=\"http://thecatapi.com/api/images/get?format=src&type=gif\"></a></span><hr></li>";
+        var listing = "<li id=\"" + i + "\">Today I learned <span class=\"content\" id=\"til" + i + "\">that every day is Caturday!<a href=\"http://thecatapi.com\"><img src=\"http://thecatapi.com/api/images/get?format=src&type=gif\"></a></span><hr></li>";
         $('#listings').prepend(listing);
     };
 }
+
 window.onload = populate;
