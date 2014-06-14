90ffa937009ed5ba04c17544d0ca022301a4b1ad
MaxWofford <max.speed.wofford@gmail.com>
2014-06-14 02:34:04 -0700

    Fixed json parsing

--- www/js/dashboard.js
+++ www/js/dashboard.js
@@ -20,18 +20,17 @@ var populate = function() {
     
     getLocation();
     
-    var json = [{"id":"3","username":"Ben","userid":"539b8d2092d6c","text":"Test","tags":"Test,tester","geoloc":"{\"latitude\":29.5083464,\"longitude\":-98.394077}","likedby":"0","example":""},{"id":"4","username":"Ben2","userid":"539be1a799fec","text":"First Test","tags":"tnk","geoloc":"{\"latitude\":29.508391300000003,\"longitude\":-98.3940331}","likedby":"0","example":""},{"id":"5","username":"Ben2","userid":"539be1a799fec","text":"First Test","tags":"tnk","geoloc":"{\"latitude\":29.508372299999998,\"longitude\":-98.3941035}","likedby":"0","example":""}]
+    var json = [{"id":"3","username":"Ben","userid":"539b8d2092d6c","text":"Test","tags":"Test,tester","geoloc":"{\"latitude\":29.5083464,\"longitude\":-98.394077}","likedby":"0","example":""},{"id":"4","username":"Ben2","userid":"539be1a799fec","text":"Firmmmmmmmmmmst Test","tags":"tnk","geoloc":"{\"latitude\":29.508391300000003,\"longitude\":-98.3940331}","likedby":"0","example":""},{"id":"5","username":"Ben2","userid":"539be1a799fec","text":"agweigwk Test","tags":"tnk","geoloc":"{\"latitude\":29.508372299999998,\"longitude\":-98.3941035}","likedby":"0","example":""}]
     
-
-    for (var i = 0; i < json.length ; i++) {
-        var listing = "<li id=\"" + i + "\"><span class=\"content\" id=\"til" + i + "\">" + $.parseJSON("text") + "</span><hr></li>";
+    for (var i = 1; i < json.length + 1 ; i++) {
+        var listing = "<li id=\"" + i + "\"><span class=\"content\" id=\"til" + i + "\">" + (json[i].text) + "</span><hr></li>";
         $('#listings').prepend(listing);
     };
 }
 
 function makeList(){
-    for (var i = 0; i < json.length ; i++) {
-        var listing = "<li id=\"" + i + "\"><span class=\"content\" id=\"til" + i + "\">" + $.parseJSON("text") + "</span><hr></li>";
+    for (var i = 1; i < json.length + 1 ; i++) {
+        var listing = "<li id=\"" + i + "\"><span class=\"content\" id=\"til" + i + "\">" + (json[i].text) + "</span><hr></li>";
         $('#listings').prepend(listing);
     }
 }
